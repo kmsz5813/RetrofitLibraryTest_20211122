46d877b12476486eca5b3884715e6c99508e674e
김민성 <kmsz5813@naver.com>
2021-11-24 17:14:06 +0900

    로그인한 사용자 데이터를 모든 화면에서 공유할 수 있게 세팅.  - GlobalData 클래스의 companion object로 변수로 세팅

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/ProductRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/ProductRecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.neppplus.retrofitlibrarytest_20211122.adapters
 
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -39,6 +40,18 @@ class ProductRecyclerAdapter(val mContext: Context, val mList: List<ProductData>
             rootLayout.setOnClickListener {
 
                 Toast.makeText(mContext, "${data.name} 상품 클릭됨", Toast.LENGTH_SHORT).show()
+            }
+
+            rootLayout.setOnLongClickListener {
+
+                val alert = AlertDialog.Builder(mContext)
+                alert.setTitle("상품 삭제")
+                alert.setMessage("정말 해당 상품을 삭제하겠습니까?")
+                alert.setPositiveButton("확인",null)
+                alert.setNegativeButton("취소",null)
+                alert.show()
+
+                return@setOnLongClickListener true
             }
 
         }
---
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/utils/GlobalData.kt
@@ -0,0 +1,12 @@
+package com.neppplus.retrofitlibrarytest_20211122.utils
+
+import com.neppplus.retrofitlibrarytest_20211122.datas.UserData
+
+class GlobalData {
+
+    companion object{
+
+        var loginUser : UserData? = null
+
+    }
+}
