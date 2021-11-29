6784187350cffe43257b54e5040a78e9dde01ea0
김민성 <kmsz5813@naver.com>
2021-11-29 18:14:38 +0900

    서버에서 모든 첨부가 성공되었다면  - Glide를 이용해서, 선택한 이미지를 UI에 반영

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/MyProfileFragment.kt
@@ -83,7 +83,15 @@ class MyProfileFragment : BaseFragment() {
                     ) {
                         if (response.isSuccessful){
                             Toast.makeText(mContext, "프로필 사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
-                        }else{
+
+
+//                            사용자가 선택한 사진(SelectedImageUri)을 프로필사진 뷰에 반영
+
+                            Glide.with(mContext).load(selectedImageUri).into(binding.imgProfile)
+
+
+                        }
+                        else{
                             Toast.makeText(mContext, "프로필 사진 변경에 실패했습니다.", Toast.LENGTH_SHORT).show()
 
                         }
