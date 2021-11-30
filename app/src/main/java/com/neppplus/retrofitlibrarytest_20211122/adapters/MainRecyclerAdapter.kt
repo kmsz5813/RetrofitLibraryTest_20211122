a6af3456d28c35c753f2bb1001f8f7e484d7fdbc
김민성 <kmsz5813@naver.com>
2021-11-30 14:58:19 +0900

    리싸이클러뷰 안에서 ViewPager 띄우기  - 각 페이지에 들어갈 Fragment 생성  - 한 종류의 Fragment를 여러 객체로 만들어 사용 예정

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/MainRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/MainRecyclerAdapter.kt
@@ -4,29 +4,45 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
 import com.bumptech.glide.Glide
 import com.neppplus.retrofitlibrarytest_20211122.R
 import com.neppplus.retrofitlibrarytest_20211122.datas.ReviewData
 
-class MainRecyclerAdapter(val mContext: Context, val mList : List<ReviewData>) : RecyclerView.Adapter< RecyclerView.ViewHolder >() {
+class MainRecyclerAdapter(val mContext: Context, val mList: List<ReviewData>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 //    2가지 viewholder 필요함. => 0번칸 : 상단부(Header) xml / 나머지(item)칸 : 리뷰모양 xml
 
-    inner class HeaderViewHolder(row : View) : RecyclerView.ViewHolder(row){
+    inner class HeaderViewHolder(row: View) : RecyclerView.ViewHolder(row) {
 
+        val imgCategory1 = row.findViewById<ImageView>(R.id.imgCategory1)
+        val bannerViewPager = row.findViewById<ViewPager>(R.id.bannerViewPager)
+
+        fun bind() {
+
+//            bannerViewPager.adapter = ?
+
+            imgCategory1.setOnClickListener {
+                Toast.makeText(mContext, "1번카테고리 눌림", Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
 
     }
 
-    inner class ItemViewHolder(row: View) : RecyclerView.ViewHolder(row){
+    inner class ItemViewHolder(row: View) : RecyclerView.ViewHolder(row) {
 
         val txtReviewProduct = row.findViewById<TextView>(R.id.txtReviewProduct)
         val txtReviewer = row.findViewById<TextView>(R.id.txtReviewer)
-        val imgProductImg = row.findViewById<TextView>(R.id.imgProductImg)
+//        val imgProductImg = row.findViewById<TextView>(R.id.imgProductImg)
 
-        fun bind(data: ReviewData){
+        fun bind(data: ReviewData) {
             txtReviewProduct.text = data.product.name
             txtReviewer.text = data.user.nickname
 
@@ -44,7 +60,7 @@ class MainRecyclerAdapter(val mContext: Context, val mList : List<ReviewData>) :
 //        position 0 : 맨 윗칸 => 상단 뷰
 //        position 그 외 : 목록 표시. => 리뷰 아이템
 
-        return when(position){
+        return when (position) {
 
             0 -> HEADER_VIEW_TYPE
             else -> REVIEW_ITEM_TYPE
@@ -54,14 +70,16 @@ class MainRecyclerAdapter(val mContext: Context, val mList : List<ReviewData>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
-        return when (viewType){
+        return when (viewType) {
             HEADER_VIEW_TYPE -> {
-                val row = LayoutInflater.from(mContext).inflate(R.layout.main_recycler_item_top_view, parent,false)
+                val row = LayoutInflater.from(mContext)
+                    .inflate(R.layout.main_recycler_item_top_view, parent, false)
                 HeaderViewHolder(row)
             }
             else -> {
 //                리뷰 아이템
-                val row = LayoutInflater.from(mContext).inflate(R.layout.main_recycler_item_review_item,parent,false)
+                val row = LayoutInflater.from(mContext)
+                    .inflate(R.layout.main_recycler_item_review_item, parent, false)
                 ItemViewHolder(row)
 
             }
@@ -72,16 +90,17 @@ class MainRecyclerAdapter(val mContext: Context, val mList : List<ReviewData>) :
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
-        when( holder ){
+        when (holder) {
             is HeaderViewHolder -> {
 
+                holder.bind()
 
             }
-            is ItemViewHolder-> {
+            is ItemViewHolder -> {
 
 //                리뷰 아이템 바인딩
 
-                holder.bind(mList[position-1])
+                holder.bind(mList[position - 1])
             }
 
         }
