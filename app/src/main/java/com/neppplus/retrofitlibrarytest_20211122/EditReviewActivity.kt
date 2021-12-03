70cdf7846a111cf3e8dc9b6cdf43346524c0ba89
김민성 <kmsz5813@naver.com>
2021-12-03 15:15:32 +0900

    모든 파라미터를 한번에 담을 HashMap 생성  - 서버 이름표에 맞게 RequestBody 첨부

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/EditReviewActivity.kt
@@ -27,6 +27,7 @@ import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class EditReviewActivity : BaseActivity() {
 
@@ -162,9 +163,16 @@ class EditReviewActivity : BaseActivity() {
             val scoreBody = RequestBody.create(MediaType.parse("text/plain"), rating.toString() )
             val tagListBody = RequestBody.create(MediaType.parse("text/plain"), tagStr )
 
+            val params = HashMap<String, RequestBody>()
+            params.put("product_id", productIdBody)
+            params.put("title", titleBody)
+            params.put("content", contentBody)
+            params.put("scor", scoreBody)
+            params.put("tag_list", tagListBody)
+
 
             apiService.postRequestReview(
-//                1. 일반
+                params,
 //                2. 이미지 등 파일 : Multopart.part
             ).enqueue(object : Callback<BasicResponse>{
                 override fun onResponse(
